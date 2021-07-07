f5f3e53d785b1689020d0cc4a5194f430dc9fa1f
wyx0-xyz <nathan@trioleyre.fr>
2021-07-07 12:36:39 +0200

    interpreter: add basic types

---
+++ src/interpreting/typing.rs
@@ -0,0 +1,28 @@
+use std::fmt;
+
+#[derive(Clone, Debug)]
+pub enum Value {
+    String(String),
+    Int(i32),
+    Float(f32),
+    Boolean(bool),
+    None
+}
+
+#[derive(PartialEq)]
+pub enum ScopeState {
+    Definition,
+    TopLevel
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Value::String(string) => write!(f, "{}", string),
+            Value::Int(int) => write!(f, "{}", int),
+            Value::Float(float) => write!(f, "{}", float),
+            Value::Boolean(boolean) => write!(f, "{}", boolean),
+            Value::None => write!(f, "None"),
+        }
+    }
+}
