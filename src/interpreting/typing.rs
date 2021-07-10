59edab1393ec2db623c32f988f63c4b5f70c3862
wyx0-xyz <nathan@trioleyre.fr>
2021-07-10 16:42:20 +0200

    interpreter: rename enum

--- src/interpreting/typing.rs
+++ src/interpreting/typing.rs
@@ -2,7 +2,7 @@ use crate::parsing::typing::Statement;
 use std::fmt;
 
 #[derive(Clone, Debug)]
-pub enum Value {
+pub enum Literal {
     String(String),
     Int(i32),
     Float(f64),
@@ -17,15 +17,15 @@ pub enum ScopeContext {
     TopLevel
 }
 
-impl fmt::Display for Value {
+impl fmt::Display for Literal {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Value::String(string) => write!(f, "{}", string),
-            Value::Int(int) => write!(f, "{}", int),
-            Value::Float(float) => write!(f, "{}", float),
-            Value::Boolean(boolean) => write!(f, "{}", boolean),
-            Value::Function(name, args, _) => write!(f, "<function:{}#{}>", name, args.join(", ")),
-            Value::None => write!(f, "None"),
+            Literal::String(string) => write!(f, "{}", string),
+            Literal::Int(int) => write!(f, "{}", int),
+            Literal::Float(float) => write!(f, "{}", float),
+            Literal::Boolean(boolean) => write!(f, "{}", boolean),
+            Literal::Function(name, args, _) => write!(f, "<function:{}#{}>", name, args.join(", ")),
+            Literal::None => write!(f, "None"),
         }
     }
 }
